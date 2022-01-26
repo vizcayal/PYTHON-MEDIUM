d759abdaab15ee5cb6359e8c0efcf9e1bfd480ed
Luis Vizcaya <vizcayaluis1@gmail.com>
2022-01-25 21:38:28 -0600

    "se solicita al usuario letras mientras el numero de intentos sea menor que la variable TRIES"

--- ahorcado.py
+++ ahorcado.py
@@ -1,17 +1,23 @@
+import constants
 def file_list(file_name):
+
     words = []
     file = open(file_name, "r", encoding= "utf-8")
     for line in file:
             words.append(line.strip())
     file.close()
+    
     return words
 
 
 def run():
+    i = 0
     word_list = file_list('data.txt')
     random_number = random.randint(0, len(word_list) - 1)
     word = word_list[random_number]
-    
+    while i < TRIES:
+        Letra = input("Ingrese una Letra: ")
+
 
 if __name__ == '__main__':
     run()
